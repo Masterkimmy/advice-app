5eaa6684dc6a658048b54d110aa76c70ebe3666c
Master Kimmy <hakeemkolawole62@gmail.com>
2024-05-10 18:06:50 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+// const data = {
+//     "slip": { "id": 125, "advice": "Why wait until valentines day for a romantic gesture?"}
+// }
+
+// const slip = data.slip
+
+// for(const key in slip){
+//     console.log(slip[key]);
+// }
